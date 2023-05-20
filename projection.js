29ca23a41fb19a866c040283414ec313778c3e07
iagowp <iago.wp@hotmail.com>
2023-05-20 02:22:04 -0300

    add calculators, change interest input to yearly

--- projection.js
+++ projection.js
@@ -1,8 +1,12 @@
+// how much money you will have after X amount of time
 const projector = ({initial = 0, time = 0, recurrent = 0, interest = 1}) => {
+  const percentageInterest = 1 + (interest / 100)
+  const monthlyInterest = Math.pow(percentageInterest, 1/12)
+
   let amount = initial
   for (let i = 0; i < time; i ++) {
-    amount *= interest
+    amount *= monthlyInterest
     amount += recurrent
   }
-  return Math.round(amount * 100) / 100;
+  return Math.round(amount * 100) / 100
 }
--- retire_by_yield.js
+++ retire_by_yield.js
@@ -1,7 +1,9 @@
-// how much time it will take to get where you want investing X money
-const retiring = ({initial = 0, recurrent = 0, interest = 1, goal = 0}) => {
+// how much time it will take to retire based on a desired yield
+const retiringByYield = ({initial = 0, recurrent = 0, interest = 1, projectedYield = 1, desiredIncome = 1}) => {
   const percentageInterest = 1 + (interest / 100)
   const monthlyInterest = Math.pow(percentageInterest, 1/12)
+
+  const goal = desiredIncome * 100 / projectedYield
 
   let amount = initial
   let months = 0;
